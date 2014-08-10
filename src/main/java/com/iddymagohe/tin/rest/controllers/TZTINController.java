2e6c0ed273f8457bcfac6c36bdd3b4c037fda366
Iddy Magohe <iddyiam@gmail.com>
2014-08-10 18:44:48 -0400

    Rejected commit: Default

--- src/main/java/com/iddymagohe/tin/rest/controllers/TZTINController.java
+++ src/main/java/com/iddymagohe/tin/rest/controllers/TZTINController.java
@@ -6,27 +6,26 @@ import com.iddymagohe.tin.repositories.BusinessRepository;
 import com.iddymagohe.tin.repositories.TaxRegionRepository;
 import com.iddymagohe.tin.rest.dto.BusinessEntry;
 import com.wordnik.swagger.annotations.Api;
-import com.wordnik.swagger.annotations.ApiModel;
 import com.wordnik.swagger.annotations.ApiOperation;
-import com.wordnik.swagger.annotations.ApiParam;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.HateoasPageableHandlerMethodArgumentResolver;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.hateoas.PagedResources;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.Resources;
-import org.springframework.http.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.*;
+import java.util.List;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
@@ -34,8 +33,6 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 /**
  * Created by iddymagohe on 7/26/14.
  */
-
-
 
 @Controller
 @Api(value = "businesses", description = "business API")
